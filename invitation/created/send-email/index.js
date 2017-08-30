d422179e016c7c024e20550b60333f4f24b51200
Anthony ESTEBE <anthony.estebe@gmail.com>
2017-08-30 23:58:57 +0700

    update sending mail

--- invitation/created/send-email/index.js
+++ invitation/created/send-email/index.js
@@ -1,7 +1,7 @@
 const sendgrid = require('sendgrid')
 const Helpers = require('sendgrid').mail
-
 const fromEmail = new Helpers.Email('no-reply@etherstellar.com', 'EtherStellar')
+
 const title = data => `You've been invited to the ${data.project.name} project`
 const content = data => `[${data.project.name}]... go to your dashboard... ${data.id}`
 
@@ -12,14 +12,11 @@ const generateMail = data => new Helpers.Mail(
   new Helpers.Content('text/plain', content(data))
 )
 
-const request = mail => sendgrid.emptyRequest({
-  method: 'POST',
-  path: '/v3/mail/send',
-  body: mail.toJSON()
-})
-
 module.exports = (context, callback) => {
-  const email = generateMail(context.body.data.Invitation.node)
-  sendgrid(context.secrets.SENDGRID_SECRET)
-    .API(email, callback)
-  }
+  const sendgridInstance = sendgrid(context.secrets.SENDGRID_SECRET)
+  sendgridInstance.API(sendgridInstance.emptyRequest({
+    method: 'POST',
+    path: '/v3/mail/send',
+    body: generateMail(context.body.data.Invitation.node).toJSON()
+  }), callback)
+}
