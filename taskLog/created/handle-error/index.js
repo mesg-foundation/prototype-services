165ffeace52e2f1b7407e3b32c2d38669190b923
Anthony ESTEBE <anthony.estebe@gmail.com>
2017-09-11 19:31:30 +0700

    add function to handle error when a task log does not have a 20x response code 10 times in the last 30 days

---
+++ taskLog/created/handle-error/index.js
@@ -0,0 +1,31 @@
+const createQuery = require('./createQuery')
+const request = require('./request')
+const updateTriggerStatus = require('./updateTriggerStatus')
+
+const date = shift => new Date(+new Date() - ((shift || 0) * 24 * 60 * 60 * 1000))
+const extractTotalError = x => x.data._allTaskLogsMeta.count
+const errorLimitReached = maxLimit => value => value >= maxLimit
+
+module.exports = (context, callback) => {
+  const trigger = context.body.data.TaskLog.node.trigger
+  const projectId = trigger.project.id
+  const authenticatedRequest = request({
+    'content-type': 'application/json',
+    'Authorization': `Bearer ${context.secrets.SECRET}`
+  })
+  const graphcoolQuery = authenticatedRequest(`https://api.graph.cool/simple/v1/${context.secrets.PROJECT_ID}`) 
+
+  Promise.resolve(createQuery(
+    projectId,
+    trigger.id,
+    date(context.secrets.MAX_ERROR_DURATION),
+    date())
+  )
+    .then(graphcoolQuery)
+    .then(extractTotalError)
+    .then(x => { console.log(x); return x })
+    .then(errorLimitReached(context.secrets.MAX_ERROR_COUNT))
+    .then(updateTriggerStatus(trigger.id, graphcoolQuery))
+    .then(x => callback(null, x))
+    .catch(callback)
+}
---
+++ taskLog/created/handle-error/updateTriggerStatus.js
@@ -0,0 +1,5 @@
+const request = require('./request')
+
+module.exports = (id, execute) => limitReached => limitReached && execute(`mutation {
+  updateTrigger(id: "${id}", enable: false) { id }
+}`)
