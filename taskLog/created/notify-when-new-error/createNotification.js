ed7469ef517f7175853593bbf2142505d94a143e
Anthony ESTEBE <anthony.estebe@gmail.com>
2017-09-12 16:52:01 +0700

    always create notification when trigger fails

--- taskLog/created/notify-when-new-error/createNotification.js
+++
@@ -1,9 +0,0 @@
-module.exports = (triggerId, projectId, execute) => errors => errors === 1 && execute(`mutation {
-  createNotification(
-    kind: TRIGGER_DISABLED,
-    triggerId: "${triggerId}",
-    projectId: "${projectId}"
-  ) {
-    id
-  }
-}`)
