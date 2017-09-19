1e9eda033c733a4544aa6e6dc174da1534c2bacc
Anthony ESTEBE <anthony.estebe@gmail.com>
2017-09-19 19:00:45 +0700

    use graphcool lib for error handler

--- code/subscriptions/task-log/handle-error/index.js
+++ code/subscriptions/task-log/handle-error/index.js
@@ -1,4 +1,4 @@
-const fetch = require('isomorphic-fetch')
+const fromEvent = require('graphcool-lib').fromEvent
 
 const createQuery = (projectId, triggerId, from, to) => `query {
   _allTaskLogsMeta(filter: {
@@ -16,15 +16,8 @@ const createQuery = (projectId, triggerId, from, to) => `query {
   }
 }`
 
-const request = headers => url => query => fetch(url, {
-  headers,
-  method: 'POST',
-  body: JSON.stringify({ query })
-})
-  .then(x => x.json())
-
-const updateTriggerStatus = (triggerId, projectId, execute) => limitReached => limitReached
-  ? execute(`mutation {
+const updateTriggerStatus = (triggerId, projectId, api) => limitReached => limitReached
+  ? api.request(`mutation {
     updateTrigger(
       id: "${triggerId}",
       enable: false
@@ -39,30 +32,24 @@ const updateTriggerStatus = (triggerId, projectId, execute) => limitReached => l
       id
     }
   }`)
-  : Promise.resolve({})
+  : Promise.resolve({ error: 'LIMIT_NOT_REACHED' })
 
 
 const date = shift => new Date(+new Date() - ((shift || 0) * 24 * 60 * 60 * 1000))
-const extractTotalError = x => x.data._allTaskLogsMeta.count
+const extractTotalError = x => x._allTaskLogsMeta.count
 const errorLimitReached = maxLimit => value => value >= maxLimit
 
 module.exports = event => {
   const trigger = event.data.TaskLog.node.trigger
   const projectId = trigger.project.id
-  const authenticatedRequest = request({
-    'content-type': 'application/json',
-    'Authorization': `Bearer ${process.env.GRAPHCOOL_SECRET}`
-  })
-  const graphcoolQuery = authenticatedRequest(`https://api.graph.cool/simple/v1/${process.env.GRAPHCOOL_PROJECT_ID}`)
-
-  return Promise.resolve(createQuery(
+  const api = fromEvent(event).api('simple/v1')
+  return api.request(createQuery(
     projectId,
     trigger.id,
     date(process.env.MAX_ERROR_DURATION),
     date())
   )
-    .then(graphcoolQuery)
     .then(extractTotalError)
     .then(errorLimitReached(process.env.MAX_ERROR_COUNT))
-    .then(updateTriggerStatus(trigger.id, projectId, graphcoolQuery))
+    .then(updateTriggerStatus(trigger.id, projectId, api))
 }
