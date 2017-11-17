59d020e3f1a89f615ee208ffb945d0526b12c6eb
Anthony ESTEBE <anthony.estebe@gmail.com>
2017-11-17 16:45:52 +0700

    add ethereum transaction service

---
+++ src/functions/trigger-service/services/EthereumTransaction/index.ts
@@ -0,0 +1,64 @@
+import * as HttpProvider from "ethjs-provider-http";
+import * as Eth from "ethjs-query";
+import { sign } from "ethjs-signer";
+import { toWei } from "ethjs-unit";
+import { fromEvent, GraphcoolOptions } from "graphcool-lib";
+
+interface IAddress {
+  address: string;
+  privateKey: string;
+}
+
+const providers = {
+  MAINNET: "https://mainnet.infura.io/",
+  TESTNET: "https://kovan.infura.io/",
+};
+
+const getAddress = async (api, project, chain) => {
+  const { allAddresses } = await api.request(`query {
+    allAddresses(first: 1, filter: {
+      chain: ${chain},
+      project: {
+        id: "${project.id}"
+      }
+    }) {
+      address
+      privateKey
+    }
+  }`);
+  return allAddresses[0];
+};
+
+export default async (event) => {
+  const { chain, address, amount } = event.meta;
+  const api = fromEvent(event).api("simple/v1");
+  const account = await getAddress(api, event.project, chain);
+  if (!account) {
+    throw new Error(`Cannot find address for the project ${event.project.id} for the blockchain ${chain}`);
+  }
+  console.log(account);
+
+  const eth = new Eth(new HttpProvider(providers[chain]));
+  const txData = {
+    data: "",
+    from: account.address,
+    gasPrice: (await eth.gasPrice()).toString(),
+    nonce: await eth.getTransactionCount(account.address),
+    to: address,
+    value: toWei(amount, "ether"),
+  };
+  const gas = (await eth.estimateGas(txData)).toString();
+  try {
+    const transaction = await eth.sendRawTransaction(sign({
+      ...txData,
+      gas,
+    }, account.privateKey));
+    return transaction;
+  } catch (e) {
+    if (e.value) {
+      throw new Error(e.value.message);
+    } else {
+      throw e;
+    }
+  }
+};
